d0662a7bb15a307a78591b1fb4bb011e8f3f3319
root <jenkins@example.com>
2021-07-22 19:01:29 +0530

    EKS Commit 3

--- node_app/app.js
+++ node_app/app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!, This Sample Node App'))
 app.listen(3000, () => console.log('Server ready'))
